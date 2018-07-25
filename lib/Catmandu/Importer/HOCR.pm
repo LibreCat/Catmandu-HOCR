7a2fa398fb36dc63a6c7538d1c6ba3a86ede1d7f
Nicolas Franck <nicolas.franck@ugent.be>
2018-07-25 12:21:19 +0200

    trial

--- lib/Catmandu/Importer/HOCR.pm
+++ lib/Catmandu/Importer/HOCR.pm
@@ -1,11 +1,30 @@
 package Catmandu::Importer::HOCR;
 use Catmandu::Sane;
-use Catmandu::Util qw(is_natural);
 use Moo;
-use XML::LibXML;
+use XML::LibXML::Reader;
+use Catmandu::Error;
 use namespace::clean;
 
 with "Catmandu::Importer";
+
+has reader => ( is => "lazy", init_arg => undef );
+
+has current_page => (
+    is => "rw",
+    default => sub { undef; }
+);
+
+sub _build_reader {
+    my $self = $_[0];
+    binmode $self->fh;
+    my $reader = XML::LibXML::Reader->new(
+        IO => $self->fh,
+        validation => 0,
+        load_ext_dtd => 0
+    ) or Catmandu::Error->throw("unable to read file");
+
+    $reader;
+}
 
 sub _parse_ocr_attr {
 
@@ -57,88 +76,109 @@ sub _parse_ocr_attrs {
 
 }
 
-sub _hocr_to_annotations {
-    my ( $fh, %args ) = @_;
+sub _parse_coords {
+    my $title = $_[0];
+    my %attrs = _parse_ocr_attrs( $title );
+    my $bbox = $attrs{bbox};
+    my $x = $bbox->{x1};
+    my $y = $bbox->{y1};
+    my $w = $bbox->{x2} - $bbox->{x1};
+    my $h = $bbox->{y2} - $bbox->{y1};
 
-    my @annotations;
+    +{
+        x => $x,
+        y => $y,
+        w => $w,
+        h => $h
+    };
+}
 
-    #libxml requires binmode
-    binmode $fh;
-    my $dom = XML::LibXML->load_html(
-        IO => $fh,
-        recover => 1
-    );
+sub _read {
+    my $state = $_[0]->reader->read() or return;
+    $state < 0 && Catmandu::Error->throw("error occurred during parsing of file");
+    $state;
+}
 
-    my $page_nr = 0;
+sub _next {
 
-    for my $page ( $dom->findnodes('//*[contains(@class,"ocr_page")]') ) {
+    my $self = $_[0];
+    state $start_element = XML_READER_TYPE_ELEMENT;
+    state $end_element   = XML_READER_TYPE_END_ELEMENT;
 
-        $page_nr++;
+    my $reader = $self->reader();
 
-        my $page_title = $page->findvalue('@title');
+    my $line;
 
-        my %page_attrs = _parse_ocr_attrs( $page_title );
-        my $page_width = $page_attrs{bbox}->{x2} - $page_attrs{bbox}->{x1};
-        my $page_height = $page_attrs{bbox}->{y2} - $page_attrs{bbox}->{y1};
+    while( $self->_read ){
 
-        #between ocr_page and ocr_line can be ocr_block or ocr_par
-        for my $line ( $page->findnodes( './/*[contains(@class,"ocr_line")]' ) ) {
+        my $name = $reader->name();
+        my $nodeType = $reader->nodeType();
+        my $class = $reader->getAttribute("class");
 
-            my $line_title = $line->findvalue('@title');
-            my %line_attrs = _parse_ocr_attrs( $line_title );
-            my $line_bbox = $line_attrs{bbox};
-            my $line_x = $line_bbox->{x1};
-            my $line_y = $line_bbox->{y1};
-            my $line_width = $line_bbox->{x2} - $line_bbox->{x1};
-            my $line_height = $line_bbox->{y2} - $line_bbox->{y1};
+        if ( $nodeType == $start_element && $class eq "ocr_page" ) {
 
-            my @words;
+            my $old_page = $self->current_page // +{ no => 0 };
 
-            #google supports ocrx_word, not ocr_word
-            for my $word (
-                $line->findnodes( './/*[contains(@class,"ocr_word")]' ),
-                $line->findnodes( './/*[contains(@class,"ocrx_word")]' )
-            ) {
+            my $title = $reader->getAttribute("title");
+            my $coords = _parse_coords( $title );
 
-                push @words, $word->textContent();
+            $self->current_page({
+                no => $old_page->{no} + 1,
+                %$coords
+            });
+
+            next;
+
+        }
+        elsif ( $nodeType == $start_element && $class eq "ocr_line" ) {
+
+            my $title = $reader->getAttribute("title");
+            my $coords = _parse_coords( $title );
+
+            my $current_page = $self->current_page();
+            $line = +{
+                page => $current_page->{no},
+                page_x => $current_page->{x},
+                page_y => $current_page->{y},
+                page_w => $current_page->{w},
+                page_h => $current_page->{h},
+                %$coords,
+                text => []
+            };
+
+        }
+        elsif ( $nodeType == $start_element && ($class eq "ocr_word" || $class eq "ocrx_word") ) {
+
+            #skip this element
+            $reader->read();
+            if( $line ) {
+                push @{$line->{text}}, $reader->value();
+            }
+            #skip end element
+            $reader->read();
+        }
+        elsif ( defined($line) && $nodeType == $end_element ) {
+
+            if ( $line ) {
+
+                $line->{text} = join("", @{ $line->{text} });
 
             }
+            last;
 
-            my $x = int($line_x);
-            my $y = int($line_y);
-            my $w = int($line_width);
-            my $h = int($line_height);
-
-
-            push @annotations,{
-                text => join(' ',@words),
-                x => $x,
-                y => $y,
-                w => $w,
-                h => $h,
-                page => $page_nr,
-                page_w => $page_width,
-                page_h => $page_height
-            };
         }
 
     }
 
-    \@annotations;
-
+    $line;
 }
+
 sub generator {
 
     my $self = $_[0];
 
     sub {
-        state $annotations;
-
-        unless ( defined( $annotations ) ) {
-            $annotations = _hocr_to_annotations( $self->fh );
-        }
-
-        shift @$annotations;
+        $self->_next();
     };
 
 }
